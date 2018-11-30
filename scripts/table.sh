1178794c1c8b1b6f3e97a515222fdb5bd763395f
JRMeyer <joshuameyer@email.arizona.edu>
2018-11-30 05:31:44 +0300

    create table for latex

--- scripts/table.sh
+++ scripts/table.sh
@@ -1,3 +1,10 @@
+export LC_NUMERIC="en_US.UTF-8"
+
+round()
+{
+echo $(printf %.2f $1)
+};
+
 TMPDIR=/tmp
 for lang in br ca cy cv de fr it kab ky tt tr ; do
 	# if [[ ! -d ../$lang ]]; then
@@ -7,12 +14,14 @@ for lang in br ca cy cv de fr it kab ky tt tr ; do
 	echo -n "" > $TMPDIR/$lang.dat
 	wer_baseline=$(cat ../$lang/LOG.$lang.scratch | grep 'Test - ' | grep -o 'WER:[^,]\+' | cut -f2 -d':' | sed 's/ *//g'); 
 	cer_baseline=$(cat ../$lang/LOG.$lang.scratch | grep 'Test - ' | grep -o 'CER:[^,]\+' | cut -f2 -d':' | sed 's/ *//g');
+    row="$lang & $(round $cer_baseline)"
 	for model in 1 2 3 4 5; do
 		wer=$(cat ../$lang/LOG.$lang.$model | grep 'Test - ' | grep -o 'WER:[^,]\+' | cut -f2 -d':' | sed 's/ *//g');
 		cer=$(cat ../$lang/LOG.$lang.$model | grep 'Test - ' | grep -o 'CER:[^,]\+' | cut -f2 -d':' | sed 's/ *//g');
-
+        row="$row & $(round $cer)" 
 		echo -e "$lang\t$model\t$wer\t$cer\t$wer_baseline\t$cer_baseline" >> $TMPDIR/$lang.dat
 	done
+    echo $row >> table.LATEX
 	cat results.gnu | sed "s/%LANG%/$lang/g" > $TMPDIR/$lang.gnu
 	gnuplot $TMPDIR/$lang.gnu
 done
